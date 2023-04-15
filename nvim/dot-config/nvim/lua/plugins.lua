e368fd8a47c733670eb32b3acaead1f7df5a6ecd
name <email>
2023-04-15 18:49:03 -0400

    load session on startup and save session on exit

--- nvim/dot-config/nvim/lua/plugins.lua
+++ nvim/dot-config/nvim/lua/plugins.lua
@@ -372,18 +372,24 @@ return {
       utils.nnoremap('<leader>fp', function() vim.cmd("Telescope projections") end)
 
       -- Autostore session on VimExit
-      --local Session = require("projections.session")
-      --vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
-      --  callback = function() Session.store(vim.loop.cwd()) end,
-      --})
+      local Session = require("projections.session")
+      vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+        callback = function() Session.store(vim.loop.cwd()) end,
+      })
 
-      ---- Switch to project if vim was started in a project dir
-      --local switcher = require("projections.switcher")
-      --vim.api.nvim_create_autocmd({ "VimEnter" }, {
-      --  callback = function()
-      --    if vim.fn.argc() == 0 then switcher.switch(vim.loop.cwd()) end
-      --  end,
-      --})
+      -- Auto Load session on VimEnter
+      vim.api.nvim_create_autocmd({ "VimEnter" }, {
+        callback = function()
+          if vim.fn.argc() ~= 0 then return end
+          local session_info = Session.info(vim.loop.cwd())
+          if session_info == nil then
+            Session.restore_latest()
+          else
+            Session.restore(vim.loop.cwd())
+          end
+        end,
+        desc = "Restore last session automatically"
+      })
     end
   }
 }
