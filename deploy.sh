73724667c434938a8e76c503da4b25a7f8d6e52c
Jeremy Hofer <fuego0607@gmail.com>
2023-04-15 17:53:23 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+action="${1}"
+domain="${2}"
+
+if [ "$action" = "D" ] || [ "$action" = "S" ] || [ "$action" = "R" ]; then
+    projects=""
+
+    if [ "$domain" = "h" ]; then
+        projects="alacritty git i3 nvim ssh tmux X yamllint zsh"
+    elif [ "$domain" = "w" ]; then
+        projects="git nvim ssh tmux yamllint zsh"
+    else
+        echo "Valid Domains: h (home) w (work)"
+        exit 1
+    fi
+
+    # shellcheck disable=SC2086
+    stow -t ~ "-$action" $projects
+else
+    echo "Valid Commands: S (stow) R (restow) D (delete)"
+    exit 1
+fi
