6cdc7889bc1992de0100c512aa027f1c183ccb0f
Jeremy Hofer <jhofer0607@gmail.com>
2025-10-06 15:11:43 -0400

    xbindkeys / i3 backlight changes. lazy snacks keybinds and config

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>msd", function()
+  Snacks.words.disable()
+end, { desc = "Disable Snacks Words" })
+map("n", "<leader>mse", function()
+  Snacks.words.enable()
+end, { desc = "Enable Snacks Words" })
