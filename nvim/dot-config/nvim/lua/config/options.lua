4ef467a98399efa44280e2aade5e039fc1fb53d0
Jeremy Hofer <jhofer0607@gmail.com>
2026-03-24 08:27:01 -0400

    update nvim stuff

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+-- lua/config/options.lua
+-- vim.filetype.add({
+--   pattern = {
+--     -- Match any file ending in .component.html
+--     [".*%.component%.html"] = "htmlangular",
+--   },
+-- })
